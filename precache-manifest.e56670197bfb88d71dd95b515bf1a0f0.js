ca99b8e8c7dc5c7639e91c80f6eaa1abdfd6f288
Muhammad Imran <ilyasimran27@gmail.com>
2019-11-01 15:17:50 -0700

    Updates

---
+++ precache-manifest.e56670197bfb88d71dd95b515bf1a0f0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2440b16658fe44c800f5b17fe8eba4",
+    "url": "/mobishop/index.html"
+  },
+  {
+    "revision": "301c1d0c9b8621b72531",
+    "url": "/mobishop/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b311ba8ff7873b8aa08c",
+    "url": "/mobishop/static/css/main.690052bb.chunk.css"
+  },
+  {
+    "revision": "301c1d0c9b8621b72531",
+    "url": "/mobishop/static/js/2.ddc153d8.chunk.js"
+  },
+  {
+    "revision": "b311ba8ff7873b8aa08c",
+    "url": "/mobishop/static/js/main.72e34823.chunk.js"
+  },
+  {
+    "revision": "4cfb115addf5c4e2478a",
+    "url": "/mobishop/static/js/runtime-main.cd281140.js"
+  },
+  {
+    "revision": "acb4200b55f7593d6dba955d64359c40",
+    "url": "/mobishop/static/media/logo.acb4200b.svg"
+  },
+  {
+    "revision": "ca9309d2d304d71e7d73b4f10357d4bd",
+    "url": "/mobishop/static/media/logo.ca9309d2.png"
+  },
+  {
+    "revision": "d63d9eff35aa057bab9c5041938d6d69",
+    "url": "/mobishop/static/media/product_1.d63d9eff.png"
+  },
+  {
+    "revision": "ad7ef891046b2a56aa41db25cbcf2a9b",
+    "url": "/mobishop/static/media/product_2.ad7ef891.png"
+  },
+  {
+    "revision": "d27ec2bc66073aa4b0b80368a9ac410e",
+    "url": "/mobishop/static/media/product_3.d27ec2bc.png"
+  },
+  {
+    "revision": "d9ed75372e686c2f8c38a53a52482e66",
+    "url": "/mobishop/static/media/product_4.d9ed7537.png"
+  },
+  {
+    "revision": "8f9a1f89508713ff90dc1b497c92b1b2",
+    "url": "/mobishop/static/media/product_5.8f9a1f89.png"
+  },
+  {
+    "revision": "b49d1b7cf1b82dad0a8059302706685a",
+    "url": "/mobishop/static/media/product_6.b49d1b7c.png"
+  },
+  {
+    "revision": "4c6ab223c32bec22d9cd6070caaedf19",
+    "url": "/mobishop/static/media/product_7.4c6ab223.png"
+  },
+  {
+    "revision": "d390282f89018d8777d36210b7df6d7b",
+    "url": "/mobishop/static/media/product_8.d390282f.png"
+  }
+]);
